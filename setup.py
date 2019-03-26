aa30c6c6f598e83328544d91871c7e3a07256208
Reinder Feenstra <r.feenstra@tiepie.nl>
2019-03-26 14:20:15 +0100

    Added PyLibTiePie v0.7 sources

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='python-libtiepie',
+      version='0.7',
+      description='Python bindings for LibTiePie',
+      license='MIT',
+      author='TiePie engineering',
+      author_email='support@tiepie.nl',
+      url='https://github.com/TiePie/python-libtiepie',
+      packages=['libtiepie'])
