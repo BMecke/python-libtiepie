ecbd1f4a2c6c3b12cc728fd32a8107e0f3728c35
Reinder Feenstra <r.feenstra@tiepie.nl>
2020-03-10 14:17:24 +0100

    Bumped version to 0.9.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='python-libtiepie',
-      version='0.9.4',
+      version='0.9.5',
       description='Python bindings for LibTiePie',
       license='MIT',
       author='TiePie engineering',
