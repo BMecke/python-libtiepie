a7fa4982d3c2d9d32f7f1c25bdcc069e91ead2bf
Dennis IJsebaart <d.ijsebaart@tiepie.nl>
2021-04-08 14:32:25 +0200

    Added python 3.9

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(name='python-libtiepie',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           'Topic :: Scientific/Engineering',
           'Topic :: Software Development',
       ])
