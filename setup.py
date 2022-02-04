d47dd20fd2adf67bf41ab541ffb319c4fe502962
Dennis IJsebaart <d.ijsebaart@tiepie.nl>
2022-02-04 10:04:57 +0100

    Bump version to v0.9.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='python-libtiepie',
-      version='0.9.8',
+      version='0.9.9',
       description='Python bindings for LibTiePie',
       license='MIT',
       author='TiePie engineering',
